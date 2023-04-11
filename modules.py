5fd7784c5c2662a2a3031a135a9361b5c807fbfa
aleynakaya11 <129987872+aleynakaya11@users.noreply.github.com>
2023-04-11 12:42:21 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,14 @@
+from matematik import topla as toplamaIslemi
+from day2 import sayHello,Human
+#built-in modules
+import random
+from selenium import webdriver
+#package
+print(toplamaIslemi(10,20))
+
+print(random,randint(0,100))
+
+human1=Human("Mirza")
+human1.talk("Merhaba")
+
+chromeDriver=webdriver.Chrome()
